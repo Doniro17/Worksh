69b06d54331b8f24e25bf5888bff4776f994ec03
Danil Afanasev <doniro.ru@gmail.com>
2018-11-14 15:40:44 +0600

    initial commit

--- little_task/HomeWork.java
+++
@@ -1,9 +0,0 @@
-package it.sevenbits.little_task;
-
-public class HomeWork {
-    public static void strBuild (){
-        StringBuilder s1 = new StringBuilder("Danil");
-        s1.append(" Afanasev");
-        System.out.println(s1);
-    }
-}
