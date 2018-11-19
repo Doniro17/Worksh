8152d3619d09b44e04e7d0342d35867a72710d3d
Danil Afanasev <doniro.ru@gmail.com>
2018-11-19 13:54:48 +0600

    'n' removed

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,14 +1,12 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Example {
 
-    public int getLongestLineCharsCount (FileReader reader) throws IOException {
+    public int getLongestLineCharsCount(FileReader reader) throws IOException {
         int maxLength = -1;
         List<String> list = new ArrayList<>();
-        int n = 0;
 
         while (reader.hasMoreLines()) {
             list.add(reader.readLine());
