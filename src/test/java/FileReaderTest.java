c1ba975478b1fb9ba4688c546fbb5375cfbd56e9
Danil Afanasev <doniro.ru@gmail.com>
2018-11-19 13:53:21 +0600

    readlinetest complite

--- src/test/java/FileReaderTest.java
+++ src/test/java/FileReaderTest.java
@@ -6,13 +6,13 @@ import static org.junit.Assert.assertEquals;
 public class FileReaderTest {
 
     @org.junit.Test
-    public void readLine() throws IOException {
+    public void readLineTest() throws IOException {
 
         String expected = "example Line 2: А может и нет";
         File file = new File("src/test/resources/example");
         FileReader reader = new FileReader(file);
         reader.readLine();
-
-        //assertEquals(expected, actual);
+        String actual = reader.readLine();
+        assertEquals(expected, actual);
     }
 }
