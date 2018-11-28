c366521150cfbd8315b297a5f55f591815b1dfba
Danil Afanasev <doniro.ru@gmail.com>
2018-11-28 20:58:39 +0600

    ArrayCheck added

--- src/main/java/ArrayCheck.java
+++ src/main/java/ArrayCheck.java
@@ -1,13 +1,22 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ArrayCheck {
 
-    public List<int[]> ArrayContainsArrayCheck (List<int[]> intList, int[] array) {
-        List<int[]> checkedList = new ArrayList<>();
+    public List<Set> SetsContainsSet(List<Set> intList, Set<Integer> array) {
+        List<Set> checkedList = new ArrayList<>();
 
-
-
+        for (Set aSet : intList) {
+            for (int i = 0; i < array.size(); i++) {
+                if (aSet.contains(array.toArray()[i])) {
+                    checkedList.add(aSet);
+                    break;
+                }
+                i = 0;
+            }
+        }
         return checkedList;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,11 +11,6 @@ public class Main {
         Set<String> keySet = hashmap.keySet();
         Collection collection = hashmap.values();
 
-        StringInMap str = new StringInMap();
-        List<String> list = new ArrayList<>();
-        list.add("Luk");
-        list.add("Beg");
-        System.out.println(str.stringToMap(list).values());
 
     }
 }
