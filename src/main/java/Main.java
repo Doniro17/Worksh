1467c665b1ec4a7ace3700db87a9639027821462
Danil Afanasev <doniro.ru@gmail.com>
2018-11-28 16:38:45 +0600

    map w 3 values added

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(final String[] args) {
+        Map<String, String> hashmap = new HashMap<>();
+        hashmap.put("fst", "zaec");
+        hashmap.put("scnd", "wolk");
+        hashmap.put("thrd", "zaic");
+        Set<String> keySet = hashmap.keySet();
+        Collection collection = hashmap.values();
+
+    }
+}
