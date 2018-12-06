4547b108f8a48c0d5aefccd0794fc08165926708
Danil Afanasev <doniro.ru@gmail.com>
2018-12-07 00:30:52 +0600

    swap class and tests added

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+/**
+ * main class
+ */
+final class Main {
+    /**
+     * default constructor for main
+     */
+    private Main() {
+
+    }
+
+    /**
+     * @param args input params array
+     */
+    public static void main(final String[] args) {
+
+
+    }
+}
